4c31187aec3a69d8cd799b421c41e8541de4df87
yuanxizhang <yuanxi.zhang@gmail.com>
2019-10-20 12:30:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+function saturdayFun(sk = "roller-skate") {
+	return `This Saturday, I want to ${sk}!`;
+}
+
+function mondayWork(wk = "go to the office") {
+	return `This Monday, I will ${wk}.`
+}
+
+function wrapAdjective(wrap = "*") {
+	return function(sp = "special") {
+		return `You are ${wrap}${sp}${wrap}!`
+	}
+}
+
+
+const Calculator = {
+	"add": function add(x, y) {
+		return x + y;
+	},
+
+	"subtract": function subtract(x, y) {
+		return x - y;
+	},
+
+	"multiply": function multiply(x, y) {
+		return x * y;
+	},
+
+	"divide": function divide(x, y) {
+		return x / y;
+	}
+};
+
+function actionApplyer(s, arr) {
+	let a = s;
+	
+	arr.forEach(i => {
+		return a = i(a);
+	});
+
+	return a;
+}
