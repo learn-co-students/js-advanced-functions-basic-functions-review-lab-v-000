30971db48149adcaba51d5428c5461cc0558d808
Emi Ko <emi8emi@gmail.com>
2019-10-25 10:20:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(z="*"){
+  return function inner(x="special") {
+    return  `You are ${z}${x}${z}!`
+  }
+}
+
+let Calculator = {
+  add: function(x,y) {
+      return x+y
+    },
+  subtract: function(x,y) {
+      return x-y
+    },
+  multiply: function(x,y) {
+      return x*y
+    },
+  divide: function(x,y) {
+      return x/y
+    },
+}
+
+let actionApplyer = (x,y) => {
+  if (y.length === 0) return x
+  return (1000+(x*2))%7
+}
