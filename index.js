24755bcf512c1cff180b2b9d49f450e8b4ae0080
Rebecca Tong <rtong@buffalo.edu>
2019-11-18 17:08:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+  return 'This Saturday, I want to ' + `${activity}!`
+}
+
+function mondayWork(work="go to the office"){
+  return 'This Monday, I will ' + `${work}.`
+}
+
+function wrapAdjective(){
+  return function(word='special'){
+     'You are' +`*${word}*!`
+  }
+}
