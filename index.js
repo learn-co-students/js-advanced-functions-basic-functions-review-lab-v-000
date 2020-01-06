3f1595a4bbd7114d06ff61bf9c6cc38673d45ad4
John Blank <veloblank@gmail.com>
2020-01-06 02:50:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 function saturdayFun(activity = "roller-skate") {
-	return `This Saturday, I want to ${activity}!`;
+  return `This Saturday, I want to ${activity}!`;
 }
 
 let mondayWork = function(activity = "go to the office") {
-	return `This Monday, I will ${activity}.`;
+  return `This Monday, I will ${activity}.`;
 };
 
-function wrapAdjective(flair = "*") {
-	return function(adjective = "special") {
-		return `You are ${flair}${adjective}${flair}!`;
-	};
-}
+let wrapAdjective = function(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+};
 
 const Calculator = {
-	add: function(a, b) {
-		return a + b;
-	},
-	subtract: function(a, b) {
-		return a - b;
-	},
-	multiply: function(a, b) {
-		return a * b;
-	},
-	divide: function(a, b) {
-		return a / b;
-	}
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  }
 };
 
 function actionApplyer(start, arr_fns) {
-	let val;
-	if (arr_fns.length === 0) {
-		return start;
-	} else {
-		arr_fns.forEach(fn => {
-			val = fn(val || start);
-		});
-		return val;
-	}
+  let val;
+  if (arr_fns.length === 0) {
+    return start;
+  } else {
+    arr_fns.forEach(fn => {
+      val = fn(val || start);
+    });
+    return val;
+  }
 }
