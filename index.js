be443672dd8e98d28c2014dffe95fe701c50d006
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-17 00:44:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
