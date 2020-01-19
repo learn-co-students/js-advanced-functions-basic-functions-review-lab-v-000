3ef256894fd10d0ed5535ad8a0020990a9a508a4
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-19 11:49:13 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function saturdayFun(activity="roller-skate") {
 function mondayWork(activity="go to the office") {
   return `This Monday, I will ${activity}.`;
 }
+
+function wrapAdjective(flair="*") {
+  return function(adjective="special") {
+    return `You are ${flair}${adjective}${flair}!`
+  }
+}
+
+let Calculator = {}
+
+Calculator.add = function add(a, b) {
+  return a + b;
+};
+
+Calculator.subtract = function subtract(a, b) {
+  return a - b;
+};
+
+Calculator.multiply = function multiply(a, b) {
+  return a * b;
+};
+
+Calculator.divide = function divide(a, b) {
+  return a / b;
+};
+
+function actionApplyer(n, arrayOfFunctions) {
+  for (let i = 0; i < arrayOfFunctions.length; i++) {
+    n = (arrayOfFunctions[i])(n);
+  }
+  return n;
+};
