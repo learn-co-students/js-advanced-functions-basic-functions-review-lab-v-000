cd5b12e397dca24f730f53f01f3f7e8aca8388e3
github username <constance.hyman@mchsi.com>
2020-02-04 17:01:51 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun(activity ='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office'){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(vf='*'){
+  return function (desc = 'special'){
+    return `You are ${vf}${desc}${vf}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b){
+    return a+b;
+  },
+  subtract: function (a,b){
+    return a-b;
+  },
+  multiply: function multiply(a,b){
+    return a*b
+  },
+  divide: function divide(a, b){
+    return a/b;
+  }
+}
+
+
+function actionApplyer(int, arr){
+  for (let i = 0; i < arr.length; i++){
+    int = arr[i](int)
+  }
+  return int
+}
