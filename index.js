4cd49b27bf936cce32c24782bcc55a84221828bb
William Barela <william.barela.dev@gmail.com>
2020-02-12 17:43:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+// ES5
+/*
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+*/
+
+// ES6
+const saturdayFun = 
+(activity = "roller-skate") => {
+  return `This Saturday, I want to ${activity}!`
+  
+}
