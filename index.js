ed6b14c73135051744fc2f44f363ccb70af849f0
sjwakeman <swakeman@ca.rr.com>
2020-05-16 07:53:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+// Implement a function called saturdayFun. It should return a String like 
+// "This Saturday, I want to ....!"
+//  Fill in the ... with the activity that's passed in as the first parameter. 
+// If nothing is passed in, default to "roller-skate".
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+// Implement a function called mondayWork. It should return a String like 
+// "This Monday, I will ... ." Fill in the ... with the activity that's passed in as the first parameter. 
+// If nothing is passed in, default to "go to the office".
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
