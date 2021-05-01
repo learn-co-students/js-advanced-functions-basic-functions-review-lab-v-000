923340a142f82eb99a55b87ead19a9cfd078c66d
Laura Messa Travin <laura.travin@gmail.com>
2021-05-01 03:42:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun( value='roller-skate'){
+//  return 'This Saturday, I want to'+value+'!' this notation does not work
+ return `This Saturday, I want to ${value}!`
+
+}
+
+let mondayWork = function(value='go to the office'){
+  return `This Monday, I will ${value}.`
+}
+
+let wrapAdjective= function(visual="*"){
+   return function(adjetive="special"){
+     return `You are ${visual}${adjetive}${visual}!`
+   }
+}
+//wrapAdjective()()
+//wrapAdjective("%")("a dedicated programmer")
