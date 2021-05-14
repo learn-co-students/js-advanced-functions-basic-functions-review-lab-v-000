c5625127561fa8cbee26c389614f008fd8c9a702
github username <github email address>
2021-05-14 17:03:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(task="go to the office") {
+  return `This Monday, I will ${task}.`
+};
+
+function wrapAdjective(flair = "*"){
+  return function innerFunction(att="special") {
+    return `You are ${flair}${att}${flair}!`
+ }
+};
+
+const Calculator = {
+    add: function(a, b) {
+        return a + b;
+    }, 
+    subtract: function(a, b){
+        return a - b
+    },
+    multiply: function(a, b){
+        return a * b
+    },
+    divide: function(a, b){
+        return a / b
+    }
+};
+
+function actionApplyer(start, array) {
+    array.forEach(func => {
+        start = func(start)
+    });
+    return start
+}
+
